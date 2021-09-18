8f62c2348efda008f7bb5caf0a59aec9d1c20e7c
zakaria chergui <z.chergui@esi-sba.dz>
2021-09-18 15:37:15 +0100

    calculat time deff

--- app/src/main/java/com/example/habit_traker/ui/Fragments/List/HabitList.kt
+++ app/src/main/java/com/example/habit_traker/ui/Fragments/List/HabitList.kt
@@ -5,17 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.habit_traker.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_habit_list.*
 
 class HabitList : Fragment() {
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_habit_list, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mainppage.setOnClickListener {
+            findNavController().navigate(R.id.action_habitList_to_habitNewItem)
+        }
+
     }
 
 
